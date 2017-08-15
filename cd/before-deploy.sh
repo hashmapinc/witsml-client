40bfc19f2a9eab995d4f3935cf79a0fe8a7148c1
Chris Herrera <chris.herrera@hashmapinc.com>
2017-08-14 22:19:12 -0500

    updating cert

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_b62105df29cb_key -iv $encrypted_b62105df29cb_iv -in cd/codesign.asc.enc -out cd/codesign.asc -d
+    openssl aes-256-cbc -K $encrypted_5d4a1b060ac6_key -iv $encrypted_5d4a1b060ac6_iv -in cd/codesign.asc.enc -out cd/codesign.asc -d
     gpg --fast-import cd/codesign.asc
 fi
